a76a8472b6b6dd6eca61200cb7cb80ce0054a66b
mikeyyb22 <mcgeheemichael22@gmail.com>
2025-12-22 22:55:56 -0300

    change file names, add py file to calculate sim strength

---
+++ cfb-playoff-sim.py
@@ -0,0 +1,11 @@
+def main ():
+    data = []
+
+def team_performance():
+    return
+
+
+indiana_name = 'Indiana Hoosiers'
+#name, ppg, tdtotal, ypg, toratio, defppg, defypg, sos, prestige
+indiana_2025 = ['Indiana Hoosiers', ]
+
