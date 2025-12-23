9741fdc2280c30ec20af1b8b8d6d7e3b6731ac4b
mikeyyb22 <mcgeheemichael22@gmail.com>
2025-12-22 23:02:59 -0300

    added Indiana 2025 stats

--- cfb-playoff-sim.py
+++ cfb-playoff-sim.py
@@ -1,11 +1,14 @@
-def main ():
-    data = []
-
 def team_performance():
     return
 
+def prestige_calculation():
+    return
 
-indiana_name = 'Indiana Hoosiers'
-#name, ppg, tdtotal, ypg, toratio, defppg, defypg, sos, prestige
-indiana_2025 = ['Indiana Hoosiers', ]
+#name, ppg, tdtotal, ypg, toratio, defppg, defypg, sos, prestige (append through prestige call)
+indiana_2025 = ['Indiana Hoosiers', 41.9, 71, 472.8, 17, 11.8, 272.2, 28]
+
+#TO-DO - FACTOR HOME/AWAY STATS
+#TO-DO - PRESTIGE CALC
+#TO-DO - PERFORMANCE EQUATION
+#STATS WEBSITE: https://www.teamrankings.com/ncf/stats/
 
