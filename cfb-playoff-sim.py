b2f6d1565c2f090aa6efb5d296c44a7497b0df0d
mikeyyb22 <mcgeheemichael22@gmail.com>
2025-12-23 22:01:12 -0300

    Created json file for team stats

--- cfb-playoff-sim.py
+++ cfb-playoff-sim.py
@@ -5,7 +5,6 @@ def prestige_calculation():
     return
 
 #name, ppg, tdtotal, ypg, toratio, defppg, defypg, sos, prestige (append through prestige call)
-indiana_2025 = ['Indiana Hoosiers', 41.9, 71, 472.8, 17, 11.8, 272.2, 28]
 
 #TO-DO - FACTOR HOME/AWAY STATS
 #TO-DO - PRESTIGE CALC
