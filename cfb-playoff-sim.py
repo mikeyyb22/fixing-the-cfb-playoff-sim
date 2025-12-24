6b2a24f999bcd7de5982d44ea700cd621ef43c93
mikeyyb22 <mcgeheemichael22@gmail.com>
2025-12-23 23:56:56 -0300

    updated to-do

--- cfb-playoff-sim.py
+++ cfb-playoff-sim.py
@@ -4,10 +4,10 @@ def team_performance():
 def prestige_calculation():
     return
 
-#name, ppg, tdtotal, ypg, toratio, defppg, defypg, sos, prestige (append through prestige call)
 
-#TO-DO - FACTOR HOME/AWAY STATS
-#TO-DO - PRESTIGE CALC
+#TO-DO - READ JSON AND SORT TEAMS BY STATISTIC
+#TO-DO - FACTOR HOME/AWAY STATS (?)
+#TO-DO - PRESTIGE CALC - APPEND TO END OF JSON
 #TO-DO - PERFORMANCE EQUATION
 #STATS WEBSITE: https://www.teamrankings.com/ncf/stats/
 
